d2e0c51bc0f015a981368f3e0c53f8ceb84ab949
avbernat <57693577+avbernat@users.noreply.github.com>
2021-11-03 19:11:49 -0500

    Experiment was started on Nov 1 and datasheets have been prepped and generated.

--- pairs.R
+++ pairs.R
@@ -1,4 +1,8 @@
 
+library(dplyr)
+
+gitpath = "/Users/anastasiabernat/Desktop/git_repositories/warming-SBB/"
+
 # egg tube data
 
 d = read.csv("/Users/anastasiabernat/Desktop/git_repositories/extreme-flight-trials/data/egg_tube_data.csv")
@@ -20,15 +24,35 @@ sample(c("M", "F"), 1) # which pull females and which males?
 # MID 16 = males
 
 MID114 = data[data$MID == 114,]
-nrow(MID114[MID114$sex == "F",]) 
+nrow(MID114[MID114$sex == "F",]) #23
 
 MID16 = data[data$MID == 16,]
-nrow(MID16[MID16$sex == "M",]) 
+nrow(MID16[MID16$sex == "M",]) #37
+
+path114 = paste0(gitpath, "datasheets/MID114.csv")
+path16 = paste0(gitpath, "datasheets/MID16.csv")
+#write.csv(MID114, file= path114, row.names = F)
+#write.csv(MID16, file= path16, row.names = F)
+
+checked114 = read.csv(paste0(gitpath, "datasheets/MID114-B.csv"))
+checked16 = read.csv(paste0(gitpath, "datasheets/MID16-B.csv"))
+
+
+filtered114 = checked114[checked114$keep !="no",]
+filtered16 = checked16[checked16$keep !="no",]
+
+dF = filtered114[filtered114$sex == "F",]
+dM = filtered16[filtered16$sex == "M",]
+
 
 # randomly select 20
 
 females = MID114[MID114$sex == "F",]$gen1_ID
 males = MID16[MID16$sex == "M",]$gen1_ID
+
+females = dF$gen1_ID
+males = dM$gen1_ID
+
 fem = sample(females, 20, replace=F)
 male = sample(males, 20, replace=F)
 
@@ -39,18 +63,19 @@ colnames(pairs) = c("MIDF","females", "males", "MIDM")
 
 pairs$wing_dateF = NA
 for (i in 1:(length(fem))){
-  row = which(MID114[MID114$sex == "F",]$gen1_ID == fem[i])
+  row = which(dF[dF$sex == "F",]$gen1_ID == fem[i])
+  print(row)
   
-  wingdate = MID114[row,]$wing_date
+  wingdate = dF[row,]$wing_date
   print(wingdate)
   pairs$wing_dateF[i] = wingdate
 }
 
 pairs$wing_dateM = NA
 for (i in 1:(length(male))){
-  row = which(MID16[MID16$sex == "M",]$gen1_ID == male[i])
+  row = which(dM[dM$sex == "M",]$gen1_ID == male[i])
   
-  wingdate = MID16[row,]$wing_date
+  wingdate = dM[row,]$wing_date
   print(wingdate)
   pairs$wing_dateM[i] = wingdate
 }
@@ -62,4 +87,7 @@ pairs$agediff = pairs$ageF - pairs$ageM
 
 pairs # not too old from each other in the pairs here. 
 
-write.csv(pairs, file= "/Users/anastasiabernat/Desktop/pairs_final.csv", row.names = F)
+outpath = paste0(gitpath, "datasheets/pairs_final-F.csv")
+
+write.csv(pairs, file=outpath, row.names = F)
+
